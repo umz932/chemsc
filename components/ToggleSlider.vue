b146d02aa160307cd63afb9cdfd6a4cc1b59bd26
umz932 <umz932@gmail.com>
2022-12-16 12:53:51 +0900

    2022/12/16: improvement

---
+++ components/ToggleSlider.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p><a @click="vis=!vis">{{title}}<b-icon pack="fas" :icon="vis?'angle-double-up':'angle-double-down'"/></a></p>
+        <vue-slide-up-down :active="vis" :duration="duration||300">
+            <div class="box">
+                <slot></slot>
+            </div>
+        </vue-slide-up-down>
+    </div>
+</template>
+
+<script>
+import VueSlideUpDown from "vue-slide-up-down";
+
+export default {
+    data: () => ({
+        vis: false,
+    }),
+    props: {
+        title: String,
+        duration: Number,
+    },
+    components: {
+        VueSlideUpDown
+    }
+}
+</script>
